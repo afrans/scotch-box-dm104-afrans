1a9a1ebb1efd59f24bcd7c7fd96256559b5280a0
Rodrigo Faria <rodrigolfsi@gmail.com>
2015-07-13 12:55:55 -0300

    add new smartphones

--- public/sql/inserts.sql
+++ public/sql/inserts.sql
@@ -39,4 +39,12 @@ insert into produto(id, nome, marca_id,	descricao, url_foto, quantidade, preco) 
 7,2899),
 (15, 'iPhone 5C 8GB', 1, 'iPhone 5C 8GB - IOS8, Tela de 4 polegadas, Cinza espacial, 8GB, Chip A6, câmera iSight de 8MP',
 'http://store.storeimages.cdn-apple.com/4662/as-images.apple.com/is/image/AppleInc/aos/published/images/i/ph/iphone5c/selection/iphone5c-selection-white-2013?wid=300&hei=300&fmt=png-alpha&qlt=95&.v=1410265644850',
-4, 1499);
+4, 1499),
+(16, 'Smartphone Nokia Lumia 520', 4, 'Smartphone Nokia Lumia 520 Desbloqueado Oi Windows Phone 8 Tela 4" 8GB 3G Wi-Fi Câmera 5MP GPS - Branco',
+'http://iacom.s8.com.br/produtos/01/00/item/113113/0/113113028_1GG.jpg', 6, 569.00),
+(17, 'Smartphone Nokia Lumia 930', 4, 'Com uma tela Full HD nítida de 5", câmera PureView de 20 MP, gravação de som Surround e carregamento sem fio integrado',
+'http://i.webapps.microsoft.com/r/image/view/-/4214630/respXLFixed/2/-/930-white-png.png', 11, 2099),
+(18, 'Smartphone Nokia Lumia 1520', 4, 'Com uma tela Full HD supersensível de 6 polegadas que permite que você veja tudo mesmo na luz do sol',
+'http://i.webapps.microsoft.com/r/image/view/-/4216452/respXLFixed/2/-/1520-white-png.png', 14, 1345.99),
+(19, 'Smartphone Nokia Lumia 735', 4, 'Apresentamos o Lumia 735. Desenvolvido para os melhores selfies', 
+'http://i.webapps.microsoft.com/r/image/view/-/4210984/respXLFixed/2/-/735-black-png.png', 3, 736.45);
