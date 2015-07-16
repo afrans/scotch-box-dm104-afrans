5d1bf364d8e84dfcd58df729366ceeb33f0591aa
Anderson Frans <afrans@inatel.br>
2015-07-16 14:32:22 -0300

    It was fixed the login rest services where return error mensagem when receive invalid data

--- public/api/index.php
+++ public/api/index.php
@@ -4,75 +4,64 @@ require 'vendor/autoload.php';
 $app = new \Slim\Slim();
 
 $app->get('/', function() {
-	echo "Welcome to SamrtPhoneStore API";
+	echo "Welcome to Guest API";
 });
 
 $app->get('/products(/:type(/:value))', function ($type = NULL, $value = NULL) use ( $app ) {
 
     $db = getDB();
 	$json_result= array();
-	
     if($type==NULL and $value==NULL){
-		foreach ($db->produto() as $produto) {
-			$json_result[]= array(
-				'id' => $produto["id"], 
-				'nome' => $produto["nome"], 
-				'marca' => $produto->marca["nome"], 
-				'url_foto' => $produto["url_foto"], 
-				'quantidade' => $produto["quantidade"], 
-				'preco' => $produto["preco"]
-			);
-		}
-		
+    foreach ($db->produto() as $produto) {
+	$json_result[]= array(
+	'id' => $produto["id"], 
+	'nome' => $produto["nome"], 
+	'marca' => $produto->marca["nome"], 
+	'url_foto' => $produto["url_foto"], 
+	'quantidade' => $produto["quantidade"], 
+	'preco' => $produto["preco"]
+	);}
     } else if ($type=="marca_id") {
-		foreach ($db->produto()->where("marca_id = ?", $value) as $produto) {
-			$json_result[]= array(
-				'id' => $produto["id"], 
-				'nome' => $produto["nome"], 
-				'marca' => $produto->marca["nome"], 
-				'url_foto' => $produto["url_foto"], 
-				'quantidade' => $produto["quantidade"], 
-				'preco' => $produto["preco"]
-			);
-		}
-		
+    foreach ($db->produto()->where("marca_id = ?", $value) as $produto) {
+	$json_result[]= array(
+	'id' => $produto["id"], 
+	'nome' => $produto["nome"], 
+	'marca' => $produto->marca["nome"], 
+	'url_foto' => $produto["url_foto"], 
+	'quantidade' => $produto["quantidade"], 
+	'preco' => $produto["preco"]
+	);}
     } else {
-		foreach ($db->produto()->where("id = ?", $value) as $produto) {
-			$json_result[]= array(
-				'id' => $produto["id"], 
-				'nome' => $produto["nome"],
-				'descricao' => $produto["descricao"],
-				'marca' => $produto->marca["nome"], 
-				'url_foto' => $produto["url_foto"], 
-				'quantidade' => $produto["quantidade"], 
-				'preco' => $produto["preco"]
-			);
-		}
-    }
-	
+    foreach ($db->produto()->where("id = ?", $value) as $produto) {
+	$json_result[]= array(
+	'id' => $produto["id"], 
+	'nome' => $produto["nome"], 
+	'marca' => $produto->marca["nome"], 
+	'url_foto' => $produto["url_foto"], 
+	'quantidade' => $produto["quantidade"], 
+	'preco' => $produto["preco"]
+	);}
+    } 
 	$app->response()->header('Content-Type', 'application/json');
 	echo json_encode($json_result);
-});
+    });
 
 $app->get('/client/:id', function($id) use ( $app ) {
 
     $db = getDB();
-	
     foreach ($db->cliente()->where("id = ?", $id) as $client) {
-		$json_result = array(
-			'nome' => $client["nome"], 
-			'sobrenome' => $client["sobrenome"], 
-			'senha' => $client["senha"], 
-			'cpf' => $client["cpf"],
-			'telefone' => $client["telefone"],
-			'email' => $client["email"], 
-			'endereco' => $client["endereco"],
-			'bairro' => $client["bairro"], 
-			'cidade' => $client["cidade"], 
-			'estado' => $client["estado"]
-		);
-	}
-	
+	$json_result = array(
+	'nome' => $client["nome"], 
+	'sobrenome' => $client["sobrenome"], 
+	'senha' => $client["senha"], 
+	'cpf' => $client["cpf"],
+    'telefone' => $client["telefone"],
+    'email' => $client["email"], 
+    'endereco' => $client["endereco"],
+    'bairro' => $client["bairro"], 
+    'cidade' => $client["cidade"], 
+	'estado' => $client["estado"]
+	);}
 	$app->response()->header('Content-Type', 'application/json');
 	echo json_encode($json_result);
 });
@@ -86,18 +75,27 @@ $app->post('/client', function () use ( $app ) {
 	echo json_encode($client);
 });
 
-$app->post('/login(/:email(/:senha))', function($email, $senha) use ( $app ) {
+$app->get('/login(/:email(/:senha))', function($email = NULL, $senha = NULL) use ( $app ) {
 
     $db = getDB();
+	$json_result = NULL;
     foreach ($db->cliente()->where("email = ? AND senha = ?" , $email, $senha) as $client) { 
-		$json_result = array (
-			'nome' => $client["nome"], 
-			'sobrenome' => $client["sobrenome"]
-		);
+	$json_result = array (
+	'nome' => $client["nome"], 
+	'sobrenome' => $client["sobrenome"]
+	);}
+	if ($json_result==NULL){
+		$json_result = array ('login' => 'Email/Senha inválidos');
 	}
 	$app->response()->header('Content-Type', 'application/json');
 	echo json_encode($json_result);
 });
+
+/*
+$app->delete('/guest/:id', function($id) use ( $app ) { 
+	echo $id;
+});
+*/
 
 function getConnection() {
 	$dbhost = 'localhost';
