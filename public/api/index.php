9ceadc91c618f6eaf2b95b21f46fbc2e74946952
afrans <andersonfrans@gmail.com>
2015-07-19 01:59:22 -0300

    Updated one case of foreach to fetch()

--- public/api/index.php
+++ public/api/index.php
@@ -12,35 +12,35 @@ $app->get('/products(/:type(/:value))', function ($type = NULL, $value = NULL) u
     $db = getDB();
 	$json_result= array();
     if($type==NULL and $value==NULL){
-    foreach ($db->produto() as $produto) {
-	$json_result[]= array(
-	'id' => $produto["id"], 
-	'nome' => $produto["nome"], 
-	'marca' => $produto->marca["nome"], 
-	'url_foto' => $produto["url_foto"], 
-	'quantidade' => $produto["quantidade"], 
-	'preco' => $produto["preco"]
-	);}
+		foreach ($db->produto() as $produto) {
+		$json_result[]= array(
+		'id' => $produto["id"], 
+		'nome' => $produto["nome"], 
+		'marca' => $produto->marca["nome"], 
+		'url_foto' => $produto["url_foto"], 
+		'quantidade' => $produto["quantidade"], 
+		'preco' => $produto["preco"]
+		);}
     } else if ($type=="marca_id") {
-    foreach ($db->produto()->where("marca_id = ?", $value) as $produto) {
-	$json_result[]= array(
-	'id' => $produto["id"], 
-	'nome' => $produto["nome"], 
-	'marca' => $produto->marca["nome"], 
-	'url_foto' => $produto["url_foto"], 
-	'quantidade' => $produto["quantidade"], 
-	'preco' => $produto["preco"]
-	);}
+		foreach ($db->produto()->where("marca_id = ?", $value) as $produto) {
+		$json_result[]= array(
+		'id' => $produto["id"], 
+		'nome' => $produto["nome"], 
+		'marca' => $produto->marca["nome"], 
+		'url_foto' => $produto["url_foto"], 
+		'quantidade' => $produto["quantidade"], 
+		'preco' => $produto["preco"]
+		);}
     } else {
-    foreach ($db->produto()->where("id = ?", $value) as $produto) {
-	$json_result[]= array(
-	'id' => $produto["id"], 
-	'nome' => $produto["nome"], 
-	'marca' => $produto->marca["nome"], 
-	'url_foto' => $produto["url_foto"], 
-	'quantidade' => $produto["quantidade"], 
-	'preco' => $produto["preco"]
-	);}
+		foreach ($db->produto()->where("id = ?", $value) as $produto) {
+		$json_result[]= array(
+		'id' => $produto["id"], 
+		'nome' => $produto["nome"], 
+		'marca' => $produto->marca["nome"], 
+		'url_foto' => $produto["url_foto"], 
+		'quantidade' => $produto["quantidade"], 
+		'preco' => $produto["preco"]
+		);}
     } 
 	$app->response()->header('Content-Type', 'application/json');
 	echo json_encode($json_result);
@@ -49,7 +49,7 @@ $app->get('/products(/:type(/:value))', function ($type = NULL, $value = NULL) u
 $app->get('/client/:id', function($id) use ( $app ) {
 
     $db = getDB();
-    foreach ($db->cliente()->where("id = ?", $id) as $client) {
+	$client=$db->cliente()->where("id = ?", $id)->fetch();
 	$json_result = array(
 	'nome' => $client["nome"], 
 	'sobrenome' => $client["sobrenome"], 
@@ -61,7 +61,7 @@ $app->get('/client/:id', function($id) use ( $app ) {
     'bairro' => $client["bairro"], 
     'cidade' => $client["cidade"], 
 	'estado' => $client["estado"]
-	);}
+	);
 	$app->response()->header('Content-Type', 'application/json');
 	echo json_encode($json_result);
 });
@@ -72,7 +72,6 @@ $app->post('/client', function () use ( $app ) {
 	$clientToAdd = json_decode($app->request->getBody(), true);
 	$client = $db->cliente->insert($clientToAdd);
 	$app->response->header('Content-Type', 'application/json');
-	
 	$json_result = array ('result' => 'SUCCESS');
 	echo json_encode($json_result);
 });
@@ -88,11 +87,9 @@ $app->post('/login(/:email(/:senha))', function($email = NULL, $senha = NULL) us
 			'id' => $client["id"]
 		);
 	}
-	
 	if ($json_result==NULL){
 		$json_result = array ('login' => 'Email/Senha inválidos');
 	}
-	
 	$app->response()->header('Content-Type', 'application/json');
 	echo json_encode($json_result);
 });
@@ -113,15 +110,13 @@ $app->post('/products_order', function () use ( $app ) {
 	$products_orderToAdd = json_decode($app->request->getBody(), true);
 	$products_order = $db->produtos_venda->insert($products_orderToAdd);
 	$app->response->header('Content-Type', 'application/json');
-
     if ($products_order!=false){
-	$json_result = array ('result' => 'SUCCESS');
-	echo json_encode($json_result);
+		$json_result = array ('result' => 'SUCCESS');
+		echo json_encode($json_result);
     } else {
-    $json_result = array ('result' => 'ERROR');
-	echo json_encode($json_result);
+		$json_result = array ('result' => 'ERROR');
+		echo json_encode($json_result);
     }
-
 });
 
 $app->get('/order/:id', function($client_id) use ( $app ) {
